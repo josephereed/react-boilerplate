6bd4276f32813be6a4958790a936aaffdd20c84a
Joseph Reed <jereed559@gmail.com>
2020-03-18 13:35:42 -0700

    Add private info into dev.js

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://jereed559:patssuck.@test-cluster1-efgll.mongodb.net/test?retryWrites=true&w=majority', 
-{ useNewUrlParser: true, useUnifiedTopology: true} ).then(() => console.log('DB connected')).catch(err => console.error(err))
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose')
+const { User } = require('./models/user')
+ const key = require('./config/key')
+mongoose.connect(key.mongoURI, 
+{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true } ).then(() => console.log('DB connected')).catch(err => console.error(err))
+
+
+app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    res.send('hello world')
+	res.json({ "hello ~": "Hi ~~ sadlsasdkd "})
+})
+
+app.post('/api/users/register', async (req, res) => {
+	const user = User(req.body)
+	try {
+		user.save()
+		return res.json({ success: true })
+	} catch(err) {
+		return res.json({ success: false, err })
+	}
 })
 
 app.listen(5000);
